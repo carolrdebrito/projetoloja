424e6a9fd690804d8bfbf4c020c31b9878f2ef54
Programação PHP 36129522025.3 <36129522025.3@senacrjedu.local>
2025-08-13 10:18:01 -0300

    subindo aulas do dia 13/08

---
+++ fundamentos/exercicio2.php
@@ -0,0 +1,26 @@
+<?php
+    //While
+    $x = 0;
+    while ($x <= 10){
+        echo "<br>Numéro $x";
+        $x++;
+    }
+
+    //For
+    for ($x = 0; $x <= 10; $x++){
+        echo "<br>Número $x";
+    }
+
+    //Do While -> a diferença entre ele e o while é que, mesmo que x seja maior que 10, 
+    // o do while vai printar a 1ª execução. O while nem faz o looping.
+    // é usado em login e senha em caixas eletrônicos...
+    $x = 0;
+    do {
+        echo "<br>Número $x";
+        $x++;
+    } while ($x <= 10);
+
+    // estruturas de controle: if, else, if else e switch
+    // estruturas de looping: while, for, do while
+?>
+
